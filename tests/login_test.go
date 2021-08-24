b30604ab252409d9aa30bbd8705ea3c44a2c249e
Krishnanunni B <krb@krb.pnq.csb>
2021-08-24 14:46:31 +0530

    sample unit test created

--- tests/login_test.go
+++ tests/login_test.go
@@ -7,16 +7,16 @@ import (
 
 func TestWc(t *testing.T) {
 
-	assertCorrectMessage := func(t testing.TB, actual, expected int) {
+	assertCorrectMessage := func(t testing.TB, actual, expected string) {
 		t.Helper()
 		if actual != expected {
-			t.Errorf("Actual:%d Expected:%d", actual, expected)
+			t.Errorf("Actual:%s Expected:%s", actual, expected)
 		}
 	}
 
     //Tesing all the functions against an empty file.
 	t.Run("login test", func(t *testing.T) {
-		actual := 
+		actual := login.Sample()
 		expected := "logged in"
 		assertCorrectMessage(t, actual, expected)
 	})}
