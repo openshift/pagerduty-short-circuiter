b1fffc081b45562c5cbddc2d8403737c200f1046
Krishnanunni B <46400630+Krishnanunni333@users.noreply.github.com>
2021-08-24 15:03:08 +0530

    Exp pdcli (#2)
    * Initial app scaffolding done
    
    * Update root.go
    
    * Made copyright owner Red Hat inc
    
    * Delete LICENSE
    
    * Update root.go
    
    * Added the license file
    
    * changed the structure
    
    * Add files via upload
    
    * tests folder created
    
    * test file needs updation
    
    * sample unit test created
    
    * test function named updated
    
    Co-authored-by: Krishnanunni B <krb@krb.pnq.csb>

---
+++ cmd/pdcli/login/cmd.go
@@ -0,0 +1,43 @@
+/*
+Copyright © 2021 Red Hat, Inc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package login
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// pdcli/login/cmdCmd represents the pdcli/login/cmd command
+var Cmd = &cobra.Command{
+	Use:   "login",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("logged in")
+	},
+}
+func init() {
+	//cobra.OnInitialize(initConfig)
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/openshift/pagerduty-short-circuiter
+
+go 1.15
+
+require github.com/spf13/cobra v1.2.1
