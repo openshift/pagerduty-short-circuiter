5e82b33c68d515342579d7c34c5985786d9698c8
Supreeth Basabattini <sbasabat@redhat.com>
2021-09-03 08:53:28 +0530

    sample test created

--- tests/login_test.go
+++ tests/login_test.go
@@ -6,17 +6,17 @@ import (
 
 func TestPdcli(t *testing.T) {
 
-	assertCorrectMessage := func(t testing.TB, actual, expected string) {
+	assertCorrectMessage := func(t testing.TB, actual, expected int) {
 		t.Helper()
 		if actual != expected {
-			t.Errorf("Actual:%s Expected:%s", actual, expected)
+			t.Errorf("Actual:%d Expected:%d", actual, expected)
 		}
 	}
 
 	//Tesing all the functions against an empty file.
-	t.Run("login test", func(t *testing.T) {
-		actual := "example"
-		expected := "logged in"
+	t.Run("sample test", func(t *testing.T) {
+		actual := 1
+		expected := 1
 		assertCorrectMessage(t, actual, expected)
 	})
 }
