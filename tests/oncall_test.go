ae9612ee0dc0cb9d01350976e6a098ae30481b1d
MitaliBhalla <mbhalla@redhat.com>
2021-10-20 08:12:13 +0530

    tests fully implemented for pdcli oncall

--- tests/oncall_test.go
+++ tests/oncall_test.go
@@ -76,4 +76,84 @@ var _ = Describe("pdcli oncall", func() {
 			Expect(result).To(Equal(expectedResponse))
 		})
 	})
+	When("pdcli oncall --next-oncall is run", func() {
+		It("Shows next oncall schedule of current user", func() {
+			listOnCallsResponse := &pdApi.ListOnCallsResponse{
+				OnCalls: []pdApi.OnCall{
+					{
+						Schedule: pdApi.Schedule{
+							APIObject: pdApi.APIObject{
+								Summary: "0-SREP Weekday Primary",
+							},
+						},
+						EscalationPolicy: pdApi.EscalationPolicy{
+							APIObject: pdApi.APIObject{
+								Summary: "Openshift Escalation",
+							},
+						},
+						Start: "2021-10-25T03:30:00Z",
+						End:   "2021-10-25T08:30:00Z",
+						User: pdApi.User{
+							Summary: "Red Hat SRE",
+						},
+					},
+				},
+			}
+			mockClient.EXPECT().ListOnCalls(gomock.Any()).Return(listOnCallsResponse, nil).Times(1)
+
+			expectedResponse := []pdcli.OncallUser{
+				{
+					EscalationPolicy: "Openshift Escalation",
+					OncallRole:       "0-SREP Weekday Primary",
+					Name:             "Red Hat SRE",
+					Start:            "10-25-2021 03:30 UTC",
+					End:              "10-25-2021 08:30 UTC",
+				},
+			}
+			result, err := pdcli.TeamSREOnCall(mockClient)
+
+			Expect(err).ToNot(HaveOccurred())
+			Expect(result).To(Equal(expectedResponse))
+		})
+	})
+	When("pdcli oncall --all is run", func() {
+		It("Shows current oncall users for all teams", func() {
+			listOnCallsResponse := &pdApi.ListOnCallsResponse{
+				OnCalls: []pdApi.OnCall{
+					{
+						Schedule: pdApi.Schedule{
+							APIObject: pdApi.APIObject{
+								Summary: "Primary Oncall",
+							},
+						},
+						EscalationPolicy: pdApi.EscalationPolicy{
+							APIObject: pdApi.APIObject{
+								Summary: "Team Escalation",
+							},
+						},
+						Start: "2021-10-25T03:30:00Z",
+						End:   "2021-10-25T08:30:00Z",
+						User: pdApi.User{
+							Summary: "Red Hat Engineer",
+						},
+					},
+				},
+			}
+			mockClient.EXPECT().ListOnCalls(gomock.Any()).Return(listOnCallsResponse, nil).Times(1)
+
+			expectedResponse := []pdcli.OncallUser{
+				{
+					EscalationPolicy: "Team Escalation",
+					OncallRole:       "Primary Oncall",
+					Name:             "Red Hat Engineer",
+					Start:            "10-25-2021 03:30 UTC",
+					End:              "10-25-2021 08:30 UTC",
+				},
+			}
+			result, err := pdcli.TeamSREOnCall(mockClient)
+
+			Expect(err).ToNot(HaveOccurred())
+			Expect(result).To(Equal(expectedResponse))
+		})
+	})
 })
