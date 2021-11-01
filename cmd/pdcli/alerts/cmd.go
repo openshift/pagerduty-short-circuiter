f45e1183b011e87c67130b5e255d04eb508a9790
Supreeth Basabattini <sbasabat@redhat.com>
2021-11-01 10:49:29 +0530

    moved UI internals to the start

--- cmd/pdcli/alerts/cmd.go
+++ cmd/pdcli/alerts/cmd.go
@@ -102,6 +102,11 @@ func alertsHandler(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
+	// UI internals
+	tui.Client = client
+	tui.Username = user.Name
+	tui.Columns = options.columns
+
 	// Check for incident ID argument
 	if len(args) > 0 {
 		incidentID = strings.TrimSpace(args[0])
@@ -188,11 +193,7 @@ func alertsHandler(cmd *cobra.Command, args []string) error {
 		incidentOpts.Urgencies = []string{"high"}
 	}
 
-	// UI internals
-	tui.Client = client
 	tui.IncidentOpts = incidentOpts
-	tui.Username = user.Name
-	tui.Columns = options.columns
 
 	// Fetch incidents
 	incidents, err := pdcli.GetIncidents(client, &incidentOpts)
