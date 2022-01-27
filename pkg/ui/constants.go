9d766270c19d1f140e8a9feee0ad79cceefbe041
Supreeth Basabattini <sbasabat@redhat.com>
2022-01-27 17:04:27 +0530

    modified ui constants

--- pkg/ui/constants.go
+++ pkg/ui/constants.go
@@ -8,38 +8,33 @@ const (
 	TitleFmt = " [lightcyan::b]%s "
 
 	// Table Titles
-	AlertsTableTitle          = "[ ALERTS ]"
-	ResolvedAlertsTableTitle  = "[ RESOLVED ALERTS ]"
-	TrigerredAlertsTableTitle = "[ TRIGERRED ALERTS ]"
-	HighAlertsTableTitle      = "[ TRIGERRED ALERTS - HIGH ]"
-	LowAlertsTableTitle       = "[ TRIGERRED ALERTS - LOW ]"
-	AlertMetadataViewTitle    = "[ ALERT DATA ]"
-	IncidentsTableTitle       = "[ TRIGERRED INCIDENTS ]"
-	AckIncidentsTableTitle    = "[ ACKNOWLEDGED INCIDENTS ]"
-	OncallTableTitle          = "[ ONCALL ]"
-	NextOncallTableTitle      = "[ NEXT ONCALL ]"
-	AllTeamsOncallTableTitle  = "[ ALL TEAMS ONCALL ]"
+	AlertsTableTitle         = "[ ALERTS ]"
+	HighAlertsTableTitle     = "[ HIGH ALERTS ]"
+	LowAlertsTableTitle      = "[ LOW ALERTS ]"
+	AlertMetadataViewTitle   = "[ ALERT DATA ]"
+	IncidentsTableTitle      = "[ TRIGERRED INCIDENTS ]"
+	AckIncidentsTableTitle   = "[ ACKNOWLEDGED INCIDENTS ]"
+	OncallTableTitle         = "[ ONCALL ]"
+	NextOncallTableTitle     = "[ NEXT ONCALL ]"
+	AllTeamsOncallTableTitle = "[ ALL TEAMS ONCALL ]"
 
 	// Page Titles
-	AlertsPageTitle          = "Alerts"
-	AlertDataPageTitle       = "Metadata"
-	ResolvedAlertsPageTitle  = "Resolved"
-	TrigerredAlertsPageTitle = "Trigerred"
-	HighAlertsPageTitle      = "High Alerts"
-	LowAlertsPageTitle       = "Low Alerts"
-	IncidentsPageTitle       = "Incidents"
-	AckIncidentsPageTitle    = "AckIncidents"
-	OncallPageTitle          = "Oncall"
-	NextOncallPageTitle      = "Next Oncall"
-	AllTeamsOncallPageTitle  = "All Teams Oncall"
+	AlertsPageTitle         = "Alerts"
+	AlertDataPageTitle      = "Metadata"
+	HighAlertsPageTitle     = "High Alerts"
+	LowAlertsPageTitle      = "Low Alerts"
+	IncidentsPageTitle      = "Incidents"
+	AckIncidentsPageTitle   = "AckIncidents"
+	OncallPageTitle         = "Oncall"
+	NextOncallPageTitle     = "Next Oncall"
+	AllTeamsOncallPageTitle = "All Teams Oncall"
 
 	// Footer
-	FooterText                = "[Q] Quit | [Esc] Go Back"
-	FooterTextStatus          = "[H] High Alerts | [L] Low Alerts\n"
-	FooterTextAlerts          = "[R] Refresh Alerts | [1] Resolved Alerts | [2] Trigerred Alerts | [3] Acknowledged Incidents | [4] Trigerred Incidents\n" + FooterText
-	FooterTextTrigerredAlerts = "[1] Resolved Alerts | [2] Trigerred Alerts | [3] Acknowledged Incidents | [4] Trigerred Incidents\n" + FooterTextStatus + FooterText
-	FooterTextIncidents       = "[ENTER] Select Incident  | [CTRL+A] Acknowledge Incidents\n" + FooterText
-	FooterTextOncall          = "[N] Your Next Oncall Schedule | [A] All Teams Oncall\n" + FooterText
+	FooterText            = "[Q] Quit | [Esc] Go Back"
+	FooterTextAlertStatus = "[H] High Alerts | [L] Low Alerts"
+	FooterTextAlerts      = FooterTextAlertStatus + "\n[1] Acknowledged Incidents | [2] Trigerred Incidents | [R] Refresh Alerts\n" + FooterText
+	FooterTextIncidents   = "[ENTER] Select Incident  | [CTRL+A] Acknowledge Incidents\n" + FooterText
+	FooterTextOncall      = "[N] Your Next Oncall Schedule | [A] All Teams Oncall\n" + FooterText
 
 	// Colors
 	TableTitleColor = tcell.ColorLightCyan
