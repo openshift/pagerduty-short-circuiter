1655bd33dceb7290ad6b9b4ae39dddc89b84d6b4
devppratik <ppanda@redhat.com>
2023-04-30 01:38:23 +0530

    Fixed Bug with Tab Switching overriding Alerts Page Commands

--- pkg/ui/constants.go
+++ pkg/ui/constants.go
@@ -37,14 +37,17 @@ const (
 	FooterTextTrigerredAlerts = "[1] Trigerred Alerts | [2] Acknowledged Incidents | [3] Trigerred Incidents\n" + FooterText
 	FooterTextIncidents       = "[ENTER] Select Incident | [CTRL+A] Acknowledge Incidents\n" + FooterText
 	FooterTextOncall          = "[N] Your Next Oncall Schedule | [A] All Teams Oncall\n" + FooterText
-	TerminalFooterText        = "[CTRL + N] Next Slide | [CTRL + P] Previous Slide | [CTRL + A] Add Slide | [CTRL + E] Exit Slide | [CTRL + O] New Cluster Login | [CTRL + Q] Quit "
+	TerminalFooterText        = "[CTRL + N] Next Slide | [CTRL + P] Previous Slide | [CTRL + A] Add Slide | [CTRL + E] Exit Slide | [CTRL + B] + [Num] Change to Slide with [Num]  | [CTRL + Q] Quit "
+	TerminalFooterEscapeState = "Enter the Slide Number to Switch To : "
 
 	// Colors
-	TableTitleColor = tcell.ColorLightCyan
-	BorderColor     = tcell.ColorLightGray
-	FooterTextColor = tcell.ColorGray
-	InfoTextColor   = tcell.ColorLightSlateGray
-	ErrorTextColor  = tcell.ColorRed
-	PromptTextColor = tcell.ColorLightGreen
-	LoggerTextColor = tcell.ColorGreen
+	TableTitleColor                = tcell.ColorLightCyan
+	BorderColor                    = tcell.ColorLightGray
+	FooterTextColor                = tcell.ColorGray
+	InfoTextColor                  = tcell.ColorLightSlateGray
+	ErrorTextColor                 = tcell.ColorRed
+	PromptTextColor                = tcell.ColorLightGreen
+	LoggerTextColor                = tcell.ColorGreen
+	TerminalFooterTextColor        = tcell.ColorGreen
+	TerminalFooterEscapeStateColor = tcell.ColorDarkGreen
 )
